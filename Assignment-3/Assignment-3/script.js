66178fe48f4f85e3df3cdf7b91986efaf7c989df
AshutoshRaval <122703876+AshutoshRaval@users.noreply.github.com>
2023-02-03 02:39:29 -0500

    columns collapse checking

--- Assignment-3/Assignment-3/script.js
+++ Assignment-3/Assignment-3/script.js
@@ -30,26 +30,26 @@ function showHideRow(row) {
 
 
 var selectcount = 0;
-var tableLength=Math.ceil(document.getElementById("myTable").rows.length/2);
+var tableLength = Math.ceil(document.getElementById("myTable").rows.length / 2);
 
 function rowHighLight(row) {
 
   var submitButton = document.getElementById('button');
-  var selectedRow = document.getElementById(row).rowIndex; 
+  var selectedRow = document.getElementById(row).rowIndex;
 
   if (document.getElementById(row).style.backgroundColor == 'yellow') {
     document.getElementById(row).style.backgroundColor = '';
     selectcount--;
-    document.getElementById(row).cells[8].innerHTML= "";
-    document.getElementById(row).cells[9].innerHTML= "";
+    document.getElementById(row).cells[8].innerHTML = "";
+    document.getElementById(row).cells[9].innerHTML = "";
   }
   else {
     document.getElementById(row).style.backgroundColor = 'yellow';
     selectcount++
-    document.getElementById("myTable").rows[selectedRow].cells[8].innerHTML=`<td><button id="deleteButton" onclick="deleteRow(this);">Delete</button></td>`;
-    document.getElementById("myTable").rows[selectedRow].cells[9].innerHTML=`<td><button id="editButton" onclick="editRow();">Edit</button></td>`;
+    document.getElementById("myTable").rows[selectedRow].cells[8].innerHTML = `<td><button id="deleteButton" onclick="deleteRow(this);">Delete</button></td>`;
+    document.getElementById("myTable").rows[selectedRow].cells[9].innerHTML = `<td><button id="editButton" onclick="editRow();">Edit</button></td>`;
     //document.getElementsByClassName("colDelete").setAttribute("display","revert");
-    
+
   }
 
   if (selectcount > 0) {
@@ -58,54 +58,71 @@ function rowHighLight(row) {
     submitButton.style.backgroundColor = 'orange';
     submitButton.style.borderColor = 'orange';
     submitButton.disabled = false;
+    var all_col = document.getElementsByClassName('colDelete');
+    for (var i = 0; i < all_col.length; i++) {
+      
+      if(i==0 || i==1 ||  i == ((selectedRow*2)+1)|| i == ((selectedRow*2))){
+        all_col[i].style.display = "revert";
+      }
+        
+      
+      
+    }
   }
   else {
 
     submitButton.style.backgroundColor = 'grey';
     submitButton.style.borderColor = 'grey';
     submitButton.disabled = true;
+    var all_col = document.getElementsByClassName('colDelete');
+    for (var i = 0; i < all_col.length; i++) {
+      all_col[i].style.display = "none";
+    }
+    
   }
 
 }
 
-function addStudent(){
+function addStudent() {
 
   try {
 
-  var table = document.getElementById("myTable");
-  var tableLength=Math.ceil(document.getElementById("myTable").rows.length/2);
-  var row = table.insertRow(document.getElementById("myTable").rows.length);
-  var checkbox = document.createElement("INPUT");
-  checkbox.setAttribute("type", "checkbox");
+    var table = document.getElementById("myTable");
+    var tableLength = Math.ceil(document.getElementById("myTable").rows.length / 2);
+    var row = table.insertRow(document.getElementById("myTable").rows.length);
+    var checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
 
-  //var rowno = tableLength-2;
-  row.setAttribute("id","detailsRow"+tableLength)
-  let td = document.createElement('td');
-  //td.id = 'detailsRow'+123;
-  td.innerHTML =  `<input type="checkbox" class="checkbox" onclick="rowHighLight('detailsRow`+tableLength+`');""/><br/><br/><img src="down.png" width="25px" onclick="showHideRow('hiderow`+tableLength+`');"/>`;
-  
-  row.appendChild(td); 
-  row.insertCell(1).innerHTML="Student"+" "+tableLength;
-  row.insertCell(2).innerHTML="Teacher"+" "+tableLength;
-  row.insertCell(3).innerHTML="Approved";
-  row.insertCell(4).innerHTML="Fall";
-  row.insertCell(5).innerHTML="TA";
-  row.insertCell(6).innerHTML=tableLength.toString()+(tableLength+1).toString()+(tableLength+2).toString()+(tableLength+3).toString()+(tableLength+4).toString();
-  row.insertCell(7).innerHTML="100%";
-  row.insertCell(8);
-  row.insertCell(9);
- 
-  var newRow = table.insertRow(document.getElementById("myTable").rows.length);
-  newRow.setAttribute("class","dropDownTextArea");
-  newRow.setAttribute("id","hiderow"+tableLength);
-  
-  
-  let tdData = document.createElement('td');
-  tdData.setAttribute("colspan",8);
-  tdData.innerHTML =  'Advisor:<br/><br/>Award Details<br/>Summer 1-2014(TA)<br/>Budget Number:<br/>Tuition Number:<br/>Comments:<br/><br/><br/>Award Status:<br/><br/><br/>'; 
-  newRow.appendChild(tdData); 
+    //var rowno = tableLength-2;
+    row.setAttribute("id", "detailsRow" + tableLength)
+    let td = document.createElement('td');
+    //td.id = 'detailsRow'+123;
+    td.innerHTML = `<input type="checkbox" class="checkbox" onclick="rowHighLight('detailsRow` + tableLength + `');""/><br/><br/><img src="down.png" width="25px" onclick="showHideRow('hiderow` + tableLength + `');"/>`;
 
-  alert("Record Added successfully");
+    row.appendChild(td);
+    row.insertCell(1).innerHTML = "Student" + " " + tableLength;
+    row.insertCell(2).innerHTML = "Teacher" + " " + tableLength;
+    row.insertCell(3).innerHTML = "Approved";
+    row.insertCell(4).innerHTML = "Fall";
+    row.insertCell(5).innerHTML = "TA";
+    row.insertCell(6).innerHTML = tableLength.toString() + (tableLength + 1).toString() + (tableLength + 2).toString() + (tableLength + 3).toString() + (tableLength + 4).toString();
+    row.insertCell(7).innerHTML = "100%";
+    var C8 = row.insertCell(8);
+    C8.setAttribute("class","colDelete");
+    var C9 = row.insertCell(9);
+    C9.setAttribute("class","colDelete");
+
+    var newRow = table.insertRow(document.getElementById("myTable").rows.length);
+    newRow.setAttribute("class", "dropDownTextArea");
+    newRow.setAttribute("id", "hiderow" + tableLength);
+
+
+    let tdData = document.createElement('td');
+    tdData.setAttribute("colspan", 8);
+    tdData.innerHTML = 'Advisor:<br/><br/>Award Details<br/>Summer 1-2014(TA)<br/>Budget Number:<br/>Tuition Number:<br/>Comments:<br/><br/><br/>Award Status:<br/><br/><br/>';
+    newRow.appendChild(tdData);
+
+    alert("Record Added successfully");
 
   } catch (error) {
     alert("Something went wrong!");
@@ -114,17 +131,17 @@ function addStudent(){
 }
 
 
-function deleteRow(index){
+function deleteRow(index) {
 
- 
-  var selectedRow = index.parentNode.parentNode.rowIndex; 
 
-  document.getElementById("myTable").deleteRow(selectedRow+1);
-  document.getElementById("myTable").deleteRow(selectedRow); 
+  var selectedRow = index.parentNode.parentNode.rowIndex;
+
+  document.getElementById("myTable").deleteRow(selectedRow + 1);
+  document.getElementById("myTable").deleteRow(selectedRow);
 
 }
 
-function editRow(){ 
+function editRow() {
   alert("Edit the Records");
 }
 // const table = document.getElementById('myTable"');
